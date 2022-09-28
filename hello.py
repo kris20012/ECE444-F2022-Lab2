d5586d8b0b65f603084858d0861110abedb10851
Krishanth Suthaharan <krishanth2001@live.com>
2022-09-28 16:07:09 -0400

    Reproduce example 4-7 in repo to flash name change message

--- hello.py
+++ hello.py
@@ -1,18 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 hello = Flask(__name__)
+hello.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(hello)
 moment = Moment(hello)
 
-@hello.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@hello.route('/', methods=['GET', 'POST'])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
     return render_template('index.html',
-                           current_time=datetime.utcnow())
-
-
-@hello.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+        form = form, name = session.get('name'))
