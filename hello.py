f1e296853d8c22b7c89f32f9cc72a12d4df24482
Krishanth Suthaharan <krishanth2001@live.com>
2022-09-27 21:17:11 -0400

    Updated web application to say hello to my name

--- hello.py
+++ hello.py
@@ -5,3 +5,7 @@ hello = Flask(__name__)
 @hello.route('/')
 def index():
     return "<h1>Hello World!</h1>"
+
+@hello.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
