36190266246b16fc1a697031cacdc72307f63a37
Krishanth Suthaharan <krishanth2001@live.com>
2022-09-28 15:45:21 -0400

    Completed activity number 3 to add name, navigation bar, date, and render time

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 hello = Flask(__name__)
+bootstrap = Bootstrap(hello)
+moment = Moment(hello)
 
 @hello.route('/')
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
+
 
 @hello.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
